32aaa54d590ec829b014d7c13491a8fae8a515c0
jovialp <32453962+jovialp@users.noreply.github.com>
2023-01-07 01:38:45 +0530

    2 files modified

--- component/sub/Search.js
+++ component/sub/Search.js
@@ -1,44 +1,44 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Search extends Component {
-  state = { artistQuery: "" };
+  state = { artistQuery: '' };
 
   searchArtist = () => {
     this.props.search(this.state.artistQuery);
   };
 
-  handleKeyPress = event => {
-    if (event.key === "Enter") {
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
       this.searchArtist();
     }
   };
-  updateArtistHistory = event => {
+  updateArtistHistory = (event) => {
     this.setState({ artistQuery: event.target.value });
   };
 
   render() {
     return (
       <div className="row ">
-      <div className="top-content">
-        <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center search">
-          <div className="col-xs-10">
-            <div>
-              <input
-                type="text"
-                onChange={this.updateArtistHistory}
-                onKeyPress={this.handleKeyPress}
-                required
-              />
-              <label>Search Artist Name</label>
-              <span className="border" />
+        <div className="top-content">
+          <div className="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center search">
+            <div className="d-flex justify-content-between">
+              <div className="inputWrap">
+                <input
+                  type="text"
+                  onChange={this.updateArtistHistory}
+                  onKeyPress={this.handleKeyPress}
+                  required
+                />
+                <label>Search Artist Name</label>
+                <span className="border" />
+              </div>
+              <div>
+                <button className="btn btn-primary" onClick={this.searchArtist}>
+                  <i className="glyphicon glyphicon-search" />
+                </button>
+              </div>
             </div>
           </div>
-          <div className="col-xs-2">
-            <button className="btn btn-primary" onClick={this.searchArtist}>
-              <i className="glyphicon glyphicon-search" />
-            </button>
-          </div>
-        </div>
         </div>
       </div>
     );
